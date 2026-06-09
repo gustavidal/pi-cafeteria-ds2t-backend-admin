69b5f5fe462b22cc7e50060c1807a8219144e523
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-06-09 16:48:58 -0300

    docs: alterar credenciais do database

--- model/database_config/knexConfig.js
+++ model/database_config/knexConfig.js
@@ -3,10 +3,10 @@ module.exports = {
         // ⚠️ ALTERAÇÃO: Define o cliente como 'mysql2'
         client: 'mysql2',
         connection: {
-            host: 'localhost',
+            host: 'mysql.railway.internal',
             user: 'root', // Substitua pelo seu usuário
-            password: 'bcd127', // Substitua pela sua senha
-            database: 'db_frequency80cafe',
+            password: 'QbetOhunYwgeWoQyRfACzKnICZvgEAbS', // Substitua pela sua senha
+            database: 'railway',
             port: 3306, // Porta padrão do MySQL
 
             // Opcional: Define charset (recomendado para UTF8)
